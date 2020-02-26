c729c3955e9e0081018b11c0c1f17dca749f52ca
Diego F. Goberna <github@feiss.be>
2020-02-27 00:36:20 +0100

    scaled assets by 0.3

--- src/levels.js
+++ src/levels.js
@@ -14,36 +14,36 @@ export const levels = [
   {
     targets: [
       {
-        position: { x: 1.5, y: 0.5, z: 0 },
+        position: { x: 0.4, y: 0.8, z: -0.3 },
         rotation: { x: 0, y: Math.PI / 2, z: 0 }
       }
     ],
     generators: [
       {
-        position: { x: 1, y: 2.5, z: 0 },
-        linearVelocity: { x: -2, y: 3.5, z: 0 }
+        position: { x: 0.3, y: 1.5, z: -0.3 },
+        linearVelocity: { x: -1.8, y: 2.5, z: 0 }
       }
       //{ position: { x: -1, y: 1, z: 0 } }
     ],
     elements: [
       {
         type: WOOD,
-        position: { x: -0.75, y: 1.5, z: 0 },
+        position: { x: -0.5, y: 1.2, z: -0.3 },
         rotation: { x: 0, y: 0, z: 6 }
       },
       {
         type: METAL,
-        position: { x: -0.2, y: 0.8, z: 0 },
+        position: { x: -0.2, y: 0.8, z: -0.3 },
         rotation: { x: 0, y: 0, z: -0.3 }
       },
       {
         type: RUBBER,
-        position: { x: 0.5, y: 0, z: 0 },
+        position: { x: 0.5, y: 0.8, z: -0.3 },
         rotation: { x: 0, y: 0, z: 0.1 }
       },
       {
         type: STATIC,
-        position: { x: 0.6, y: 1.8, z: 0 },
+        position: { x: 0.1, y: 0.3, z: -0.3 },
         rotation: { x: 0, y: 0, z: 0.1 }
       }
     ]
