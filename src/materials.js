4a50e6cbaf76eb8c588129474897a0415d511c8f
Fernando Serrano <fernandojsg@gmail.com>
2020-02-28 13:33:59 +0100

    reorder

--- src/materials.js
+++ src/materials.js
@@ -2,7 +2,9 @@ import * as THREE from "three";
 
 // global environment map
 
-export const environmentMap = new THREE.TextureLoader().load("assets/env.jpg");
+export const environmentMap = new THREE.TextureLoader().load(
+  "assets/textures/env.jpg"
+);
 environmentMap.mapping = THREE.EquirectangularReflectionMapping;
 environmentMap.encoding = THREE.sRGBEncoding;
 environmentMap.flipY = false;
