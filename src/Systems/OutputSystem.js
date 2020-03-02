9cc8844927addd93e1276b5eb02a65aea2c60bf6
Fernando Serrano <fernandojsg@gmail.com>
2020-03-02 11:49:09 +0100

    Output system

--- src/Systems/OutputSystem.js
+++ src/Systems/OutputSystem.js
@@ -1,36 +1,62 @@
 /* global THREE */
 import { System } from "ecsy";
-import { Element, Object3D } from "../Components/components.js";
+import { Element, BallGenerator, Target, Object3D } from "../Components/components.js";
 
 export class OutputSystem extends System {
-  execute() {
-    return; // TODO
-    if (this.events.generateJSON.length > 0) {
-      var entities = this.queries.entities;
-      var json = [];
-      for (let i = 0; i < entities.length; i++) {
-        var item = {};
-        var entity = entities[i];
-        var element = entity.getComponent(Element);
-        item.type = element.type;
+  outputJSON() {
+    var json = {
+      targets: [],
+      generators: [],
+      elements: []
+    };
 
-        var object = entity.getComponent(Object3D).value; // @todo Transform
-        item.position = JSON.parse(JSON.stringify(object.position));
-        item.rotation = JSON.parse(JSON.stringify(object.rotation));
+    this.queries.elements.results.forEach(entity => {
+      var item = {};
+      var element = entity.getComponent(Element);
+      item.type = element.type;
 
-        json.push(item);
-      }
-      console.log(JSON.stringify(json, null, 2));
-    }
+      var object = entity.getComponent(Object3D).value; // @todo Transform
+      item.position = JSON.parse(JSON.stringify(object.position));
+      item.rotation = {
+        x: object.rotation._x,
+        y: object.rotation._y,
+        z: object.rotation._z
+      };
+
+      json.elements.push(item);
+    });
+
+    this.queries.targets.results.forEach(entity => {
+      var item = {};
+      var object = entity.getComponent(Object3D).value; // @todo Transform
+      item.position = JSON.parse(JSON.stringify(object.position));
+      item.rotation = {
+        x: object.rotation._x,
+        y: object.rotation._y,
+        z: object.rotation._z
+      };
+
+      json.targets.push(item);
+    });
+
+    this.queries.generators.results.forEach(entity => {
+      var item = {};
+      var generator = entity.getComponent(BallGenerator);
+      var object = entity.getComponent(Object3D).value; // @todo Transform
+      item.position = JSON.parse(JSON.stringify(object.position));
+      item.linearVelocity = generator.linearVelocity;
+
+      json.generators.push(item);
+    });
+
+    console.log(JSON.stringify(json, null, 2));
   }
+
+  execute() {}
 }
 
 OutputSystem.queries = {
-  entities: {
-    components: [Element]
-  }
-};
-
-OutputSystem.events = {
-  generateJSON: "generateJSON"
+  elements: { components: [Element] },
+  targets: { components: [Target] },
+  generators: { components: [BallGenerator] },
 };
